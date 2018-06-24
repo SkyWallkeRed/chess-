b54637fd6c80c6351c50ab9c49d3d77008c23e86
דוד אוחנה <davidmail000@gmail.com>
2018-06-24 11:46:06 +0200

    basic templete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AccordionModule } from 'primeng/accordion';     // accordion and accordion tab
+import { MenuItem } from 'primeng/api';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BoardComponent } from './board/board.component';
+import { RowComponent } from './row/row.component';
+import { CellComponent } from './cell/cell.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    RowComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
