b69cf6eaa256007c4e1ca34447b3b1bf4c92c5eb
Ronit Herzog <lee.herzog5@gmail.com>
2018-06-24 14:20:19 +0300

    modelTs

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {Pawn} from '..model/pawn';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  public N = new Knight();
+  R = new Rook(); 
+  K = new King();
+  B = new Bishop();
+  Q = new Queen();
+ public P = new Pawn();
+  public BoardArray: Array<any> = [
+    [{R,b}, {N,b}, {B,b}, {Q,b}, {K, b}, {B,b}, {N, b}, {R, b}],
+    [{P, b}, {P, b}, {P, b}, {P, b}, {P, b}, {P, b}, {P, b},{P, b}],
+    [{},{}, {},{},{},{},{},{}],
+    [{},{}, {},{},{},{},{},{}],
+    [{},{}, {},{},{},{},{},{}],
+    [{},{}, {},{},{},{},{},{}],
+    [{P, w}, {P, w}, {P, w}, {P, w}, {P, w}, {P, w}, {P, w},{P, w}],
+    [{R,w}, {N,w}, {B,w}, {Q,w}, {K,w}, {B,w}, {N, w}, {R, w}]
+    ]
 
   constructor() { }
 }
---
+++ src/app/model/base-piece.ts
@@ -0,0 +1,4 @@
+export class Piece {
+    Public Type: string;
+
+}
