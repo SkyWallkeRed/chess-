a2bc33cefe860befa84ab71b28bdc955ac2efd41
Ronit Herzog <lee.herzog5@gmail.com>
2018-06-24 17:30:01 +0300

    correctedTileNumbers

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -24,17 +24,6 @@ export class GameService {
   constructor() {
     this.boardSubject = new Subject<any>();
     this.boardObservable = this.boardSubject.asObservable();
-
-    this.boardArray = [
-      [{ Rb }, { Nb }, { Bb }, { Qb }, { Kb }, { Bb }, { Nb }, { Rb }],
-      [{ Pb }, { Pb }, { Pb }, { Pb }, { Pb }, { Pb }, { Pb }, { Pb }],
-      [{}, {}, {}, {}, {}, {}, {}, {}],
-      [{}, {}, {}, {}, {}, {}, {}, {}],
-      [{}, {}, {}, {}, {}, {}, {}, {}],
-      [{}, {}, {}, {}, {}, {}, {}, {}],
-      [{ Pw }, { Pw }, { Pw }, { Pw }, { Pw }, { Pw }, { Pw }, { Pw }],
-      [{ Rw }, { Nw }, { Bw }, { Qw }, { Kw }, { Bw }, { Nw }, { Rw }]
-    ];
     this.getData();
   }
   getData() {
