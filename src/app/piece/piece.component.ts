261a57d5e30950eb51ada470577b2dba3c7de6c1
דוד אוחנה <davidmail000@gmail.com>
2018-06-29 16:48:59 +0200

    david fixed wepon alwis on bug , woking great

--- src/app/piece/piece.component.ts
+++ src/app/piece/piece.component.ts
@@ -9,13 +9,11 @@ import { GameService } from '../game.service';
   styleUrls: ['./piece.component.scss']
 })
 export class PieceComponent implements OnInit {
-  // @Output() bloodMarkCell: EventEmitter<any> = new EventEmitter();
   private killActive = false;
   @Input() myPiece;
   @Input() animateKill;
   @ViewChild('piece') piece: ElementRef;
   @ViewChild('wep') wep: ElementRef;
-  // @ViewChild('blood') blood: ElementRef;
   private tl = new TimelineMax();
   private bloodMark;
   private axe = false;
@@ -27,8 +25,6 @@ export class PieceComponent implements OnInit {
 
   constructor(private gameService: GameService) { }
   //
-
-
   ngOnInit() {
     //   SUBSCRIBE TO A KILL EVENT THAT WILL ANIMATE BLOOD MARK.
     this.gameService.killObservable.subscribe((data) => {
@@ -38,7 +34,10 @@ export class PieceComponent implements OnInit {
 
     if (this.myPiece !== null) {
       this.myUrl = this.myPiece.imgUrl;
-      // on board load all blood mark are activ for 3 sec|
+      // on board load all blood mark are activ for 3 sec.
+
+      // BUGGED BLOOD EFFECT NEED WORK .
+
       // setTimeout(() => {
       //   if (this.killActive) {
       //     this.bloodMark = true;
@@ -50,26 +49,22 @@ export class PieceComponent implements OnInit {
       //   this.bloodMark = false;
       // }, 3000);
 
-
+      // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
     }
 
   }
-  // tslint:disable-next-line:use-life-cycle-interface
-  ngAfterViewChecked() {
 
-  }
   // tslint:disable-next-line:use-life-cycle-interface
-  ngOnDestroy() {
+  ngOnDestroy() { // NEED TO ADD UNSUBSCRIBE ######################## .
 
   }
   // JUMP IS THE OC-CLICK OF EACH PIECE .
   jump() {
-    // console.log('jump');
+
     this.tl.to(this.piece.nativeElement, 0.2, { bottom: 20 }).to(this.piece.nativeElement, 0.2, { top: 50 });
     this.attack();
   }
   attack() {
-    // console.log('attack');
     // CHECK WHAT WEPON PER PIECE =>
     if (this.myPiece.type === 'pawn') {
       this.sword = true;
@@ -78,14 +73,21 @@ export class PieceComponent implements OnInit {
     } else {
       this.axe = true;
     }
+
     // SHOW WEPON ANIMATION =>
     TweenMax.to(this.wep.nativeElement, 1, { left: 75, bottom: 80 });
     TweenMax.to(this.wep.nativeElement, 0.5, { rotationX: '360_w', rotationY: '-60_short' });
-
+    this.clearWep();
   }
   // KILL ISNT BEEING USED ET THES MOMENT.
-  kill() {
-
+  clearWep() {
+    setTimeout(() => {
+      if (this.sword || this.axe || this.dragon) {
+        this.dragon = false;
+        this.axe = false;
+        this.sword = false;
+      }
+    }, 4000);
   }
 
 
